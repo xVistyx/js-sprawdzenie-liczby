49b89b7947c6bb11e2257c854c074420118b94be
xVistyx <wiktor.hc@gmail.com>
2022-07-18 13:19:14 +0200

    js-sprawdzenie-liczby

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let liczba = 110;
+
+function liczbaPrzedzial (_par1) {
+
+if (_par1 >= 100) {
+
+ if (_par1 <= 200) {
+
+    return "Liczba znajduje się w przedziale"
+ } 
+}
+else {
+    return "liczba nie znajduje się w przedziale"
+}
+}
+console.log(liczbaPrzedzial(liczba));
